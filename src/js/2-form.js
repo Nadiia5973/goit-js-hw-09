71a668269c49641641063b38adb28f5a19999b63
Derkach Nadiia <derkachnadiia1@gmail.com>
2025-04-13 19:25:35 +0300

    add all files

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,33 +1,38 @@
 const formData = {
-    email: "",
-    massage: ""
-}
+  email: '',
+  message: '',
+};
 const forma = document.querySelector('.feedback-form');
 const KEY = 'feedback-form-state';
-forma.addEventListener('submit', submitForm)
-forma.addEventListener('input', inputLabel)
-function inputLabel (event){
-    formData.email = event.target.form.elements.email.value;
-    formData.massage = event.target.form.elements.massage.value;
-    localStorage.setItem(KEY, JSON.stringify(formData));
+forma.addEventListener('submit', submitForm);
+forma.addEventListener('input', inputLabel);
+function inputLabel(event) {
+  formData.email = event.target.form.elements.email.value;
+  formData.message = event.target.form.elements.message.value;
+  localStorage.setItem(KEY, JSON.stringify(formData));
 }
-function submitForm(event){
-event.preventDefault();
-if(formData.email === '' || formData.massage === ''){
-    alert("Fill please all fields");
-}
-event.currentTarget.reset();
-localStorage.removeItem(KEY);
-}
-function saveText (){
-    const save = localStorage.getItem(KEY);
-    const parseSave = JSON.parse(save);
-if(save){
-   console.log(parseSave);
-   forma.elements.email.value = parseSave.email;
-   forma.elements.massage.value = parseSave.massage;
-}
-}
+function submitForm(event) {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
 
+  event.currentTarget.reset();
+  localStorage.removeItem(KEY);
+  formData.email = '';
+  formData.message = '';
+}
+function saveText() {
+  const save = localStorage.getItem(KEY);
+
+  if (save) {
+    const parseSave = JSON.parse(save);
+    forma.elements.email.value = parseSave.email;
+    forma.elements.message.value = parseSave.message;
+    formData.email = parseSave.email;
+    formData.message = parseSave.message;
+  }
+}
 
 saveText();
